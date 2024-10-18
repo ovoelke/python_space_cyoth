90da5e1e05532d2d5af150031740a73e6e4e0e3b
Oliver Völke <oliver@voelke.net>
2024-10-18 07:37:20 +0200

    Updating game code.

---
+++ __init__.py
@@ -0,0 +1,5 @@
+import Game as cyoth
+
+if __name__ == '__main__':
+    instance = cyoth.Game()
+    instance.run()
--- game.py
+++ game.py
@@ -60,7 +60,6 @@ class Game:
         pg.display.update()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     game = Game()
     game.run()
